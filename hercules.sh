8597c5050b4d2abee2d9bdfc8161f91f82265856
Marc Chisinevski <mchisine@redhat.com>
2020-07-23 23:34:55 -0700

    Update hercules.sh

--- hercules.sh
+++ hercules.sh
@@ -1,2 +1,2 @@
 buildah bud -t hercules:latest .
-sudo podman run -v .:/marc:Z  -dt --name=hercules  -p 3270:3270 -p 8038:8038 localhost/hercules
+sudo podman run -e DISPLAY  -v .:/marc:Z -v ~/.Xauthority:/root/.Xauthority:Z --net=host  -dt --name=hercules  -p 3270:3270 -p 8038:8038 localhost/hercules
